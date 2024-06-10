98fdce0437139a87eaf26eba52b2b610fed174cc
diwashtimilsina <126002522+diwashtimilsina@users.noreply.github.com>
2024-06-10 04:00:00 -0400

    new commit

---
+++ matrix.R
@@ -0,0 +1,14 @@
+#matrix in r
+m<- matrix(nrow=2,ncol = 3)
+m
+dim(m)
+
+attributes(m)
+
+#creation of matrix using dimension atribute
+n<- 1:10
+n
+
+dim(n)<-c(2,5)
+n
+
---
+++ page1.R
@@ -0,0 +1,11 @@
+#creating  vector
+x<-c(1,2,3,4,5)
+y<- as.numeric(1,4,65,4)
+z<- as.character.default(1,2,3,4,5)
+i<- as.logical(0,1,2,3,4,5)
+
+
+print(x)
+print(y)
+print(z)
+print(i)
